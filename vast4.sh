fd437000da5dfb47a5d9ed159bf27a4a0d779342
ROGBILLION <147674591+ROGBILLION@users.noreply.github.com>
2023-12-26 02:53:23 +0100

    Update vast4.sh

--- vast4.sh
+++ vast4.sh
@@ -28,7 +28,7 @@ chmod +x build.sh > /dev/null 2>&1
 ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x5372F4272B44dd424b09025298fbcAFcfc528ca9/g' config.conf > /dev/null 2>&1
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x312153fe1d3B104E6F45097313aEC5Fc8D7d828F/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
